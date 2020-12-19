d563d41e360cfb588ce54f4dc8d1b874173dca45
zacoope1 <zacoope1@asu.edu>
2020-12-18 22:44:27 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Portfolio Microservice</h1>
+<h3>@Author: Zachary Cooper</h3>
+<h4>**START COMMAND: <code>.\mvnw spring-boot:run</code></h4>
---
+++ src/main/java/com/webservice/user/UserController.java
@@ -0,0 +1,9 @@
+package com.webservice.user;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+
+}
