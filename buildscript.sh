b5f06cd0d548171a4f434561908e5954228382ee
zacoope1 <zacoope1@asu.edu>
2020-12-31 13:56:01 -0700

    Updated build script

--- buildscript.sh
+++ buildscript.sh
@@ -1,19 +1,23 @@
-BACKEND_PATH='/mnt/dev1/development/microservice/'
+MICROSERVICES_PATH='/mnt/dev1/development/microservice/'
 
 screen -S website-backend -X quit
 echo 'Terminating current build'
 
-sudo rm -rf ${BACKEND_PATH}/website-backend-old
+sudo rm -rf ${MICROSERVICES_PATH}/website-backend-old
 echo 'Removing backup.'
 
-sudo cp -r ${BACKEND_PATH}/website-backend ${BACKEND_PATH}/website-backend-old
+sudo cp -r ${MICROSERVICES_PATH}/website-backend ${MICROSERVICES_PATH}/website-backend-old
 echo 'Making new backup.'
 
-sudo rm -rf ${BACKEND_PATH}/website-backend
+sudo rm -rf ${MICROSERVICES_PATH}/website-backend
 echo 'Removing current build.'
 
-sudo cp -r website-backend/ ${BACKEND_PATH}
+sudo cp -r website-backend/ ${MICROSERVICES_PATH}
 echo 'Copying new build to current build directory.'
+
+sudo rm ${MICROSERVICES_PATH}/website-backend/src/main/resources/application.properties
+sudo cp ${MICROSERVICES_PATH}/configs/application.properties ${MICROSERVICES_PATH}/website-backend/src/main/resources/
+echo 'Replacing application.properties file'
 
 screen -S website-backend
 echo 'Created screen session'
@@ -23,14 +27,15 @@ echo 'Setting JAVA_HOME to JAVA 14.'
 
 mvn -version
 
-cd ${BACKEND_PATH}/website-backend
+cd ${MICROSERVICES_PATH}/website-backend
 
 mvn clean install
 echo 'Running maven clean install.'
 
 chmod +x mvnw
+echo 'Setting permissions to mvnw'
 
+echo 'Booting up backend.'
 ./mvnw spring-boot:run
-echo 'Running backend.'
 
-echo 'DONE'
+echo 'Done!'
