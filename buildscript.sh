f6dbc9f38d4eb9d1d2c5bec20afa91706027362a
zacoope1 <zacoope1@asu.edu>
2020-12-31 14:20:43 -0700

    Update

--- buildscript.sh
+++ buildscript.sh
@@ -3,39 +3,39 @@ MICROSERVICES_PATH='/mnt/dev1/development/microservice/'
 screen -S website-backend -X quit
 echo 'Terminating current build'
 
-sudo rm -rf ${MICROSERVICES_PATH}/website-backend-old
+sudo rm -rf ${MICROSERVICES_PATH}website-backend-old
 echo 'Removing backup.'
 
-sudo cp -r ${MICROSERVICES_PATH}/website-backend ${MICROSERVICES_PATH}/website-backend-old
+sudo cp -r ${MICROSERVICES_PATH}/website-backend ${MICROSERVICES_PATH}website-backend-old
 echo 'Making new backup.'
 
-sudo rm -rf ${MICROSERVICES_PATH}/website-backend
+sudo rm -rf ${MICROSERVICES_PATH}website-backend
 echo 'Removing current build.'
 
 sudo cp -r website-backend/ ${MICROSERVICES_PATH}
 echo 'Copying new build to current build directory.'
 
-sudo rm ${MICROSERVICES_PATH}/website-backend/src/main/resources/application.properties
-sudo cp ${MICROSERVICES_PATH}/configs/application.properties ${MICROSERVICES_PATH}/website-backend/src/main/resources/
+sudo rm ${MICROSERVICES_PATH}website-backend/src/main/resources/application.properties
+sudo cp ${MICROSERVICES_PATH}configs/application.properties ${MICROSERVICES_PATH}/website-backend/src/main/resources/
 echo 'Replacing application.properties file'
 
-screen -S website-backend
+screen -dmS website-backend bash -c 'export JAVA_HOME="/usr/lib/jvm/java-14-openjdk-amd64"; mvn-version; mvn clean install; ./mvnw spring-boot:run'
 echo 'Created screen session'
 
-export JAVA_HOME="/usr/lib/jvm/java-14-openjdk-amd64"
-echo 'Setting JAVA_HOME to JAVA 14.'
+# export JAVA_HOME="/usr/lib/jvm/java-14-openjdk-amd64"
+# echo 'Setting JAVA_HOME to JAVA 14.'
 
-mvn -version
+# mvn -version
 
-cd ${MICROSERVICES_PATH}/website-backend
+# cd ${MICROSERVICES_PATH}website-backend
 
-mvn clean install
-echo 'Running maven clean install.'
+# mvn clean install
+# echo 'Running maven clean install.'
 
-chmod +x mvnw
-echo 'Setting permissions to mvnw'
+# chmod +x mvnw
+# echo 'Setting permissions to mvnw'
 
-echo 'Booting up backend.'
-./mvnw spring-boot:run
+# echo 'Booting up backend.'
+# ./mvnw spring-boot:run
 
 echo 'Done!'
