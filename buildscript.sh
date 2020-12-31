897c91ab59040e07dbcbda63c1fd716066c1d4e8
zacoope1 <zacoope1@asu.edu>
2020-12-31 14:46:23 -0700

    updated build script

--- buildscript.sh
+++ buildscript.sh
@@ -15,6 +15,9 @@ echo 'Removing current build.'
 sudo cp -r /var/lib/jenkins/workspace/Back-End-Microservice ${MICROSERVICES_PATH}/website-backend
 echo 'Copying new build to current build directory.'
 
+sudo chmod 777 ${MICROSERVICES_PATH}/website-backend
+echo 'setting permissions to directory'
+
 sudo rm ${MICROSERVICES_PATH}website-backend/src/main/resources/application.properties
 sudo cp ${MICROSERVICES_PATH}configs/application.properties ${MICROSERVICES_PATH}website-backend/src/main/resources/
 echo 'Replacing application.properties file'
@@ -25,12 +28,7 @@ echo 'Setting JAVA_HOME to JAVA 14.'
 mvn -version
 
 cd ${MICROSERVICES_PATH}website-backend
+echo 'moving to /mnt/dev1/development/microservice/website-backend'
 
-mvn clean install 
-    && echo 'Running maven clean install.' 
-    && chmod +x mvnw 
-    && echo 'Setting permissions to mvnw' 
-    && screen -dmS website-backend bash -c './mvnw spring-boot:run; exec sh' 
-    && echo 'Created screen session' 
-    && echo 'Done!'
+mvn clean install && echo 'Running maven clean install.' && chmod +x mvnw && echo 'Setting permissions to mvnw' && screen -dmS website-backend bash -c './mvnw spring-boot:run; exec sh' && echo 'Created screen session' && echo 'Done!'
 
