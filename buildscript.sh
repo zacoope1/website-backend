8aa30f8ce3dcc164a61f515e9cb0383a69926534
zacoope1 <zacoope1@asu.edu>
2020-12-31 16:02:46 -0700

    update

--- buildscript.sh
+++ buildscript.sh
@@ -15,9 +15,13 @@ echo 'Removing current build.'
 sudo cp -r /var/lib/jenkins/workspace/Back-End-Microservice ${MICROSERVICES_PATH}/website-backend
 echo 'Copying new build to current build directory.'
 
+sudo chown -R jenkins website-backend/
+
 sudo chmod 777 ${MICROSERVICES_PATH}/website-backend
 echo 'setting permissions to directory'
 
 sudo rm ${MICROSERVICES_PATH}website-backend/src/main/resources/application.properties
 sudo cp ${MICROSERVICES_PATH}configs/application.properties ${MICROSERVICES_PATH}website-backend/src/main/resources/
 echo 'Replacing application.properties file'
+
+mvn clean install && ./mvnw spring-boot:run 
