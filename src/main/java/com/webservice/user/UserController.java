f80a79cb04fe2b575835ef37ce03592c6445e9b6
zacoope1 <zacoope1@asu.edu>
2021-02-09 18:03:23 -0700

    Enabled cors

--- src/main/java/com/webservice/user/UserController.java
+++ src/main/java/com/webservice/user/UserController.java
@@ -13,10 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -24,6 +21,7 @@ public class UserController {
 
     @Autowired UserHandler userHandler;
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @PostMapping("/user")
     public ResponseEntity<?> getUserInfo(@RequestBody UserInfoRequest request) {
 
@@ -45,6 +43,7 @@ public class UserController {
 
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @PostMapping("/create/user")
     public ResponseEntity<?> createUser(@RequestBody CreateUserRequest request){
 
